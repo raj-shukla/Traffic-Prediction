6f9a0c58b9b68d045f962710b92630abf2075069
Raj M Shukla <rshukla@0562113tgl482l.rd.unr.edu>
2018-02-27 12:26:45 -0800

    Data read cont...

--- readData.py
+++ readData.py
@@ -1,7 +1,23 @@
+import numpy as np
 import csv
 
-with open ('Data/Oct2017_Flow/pems_output_0.csv') as csvfile :
-    data = csv.reader(csvfile)
-    #for row in data:
-        #print(row)
-    print (data)
+
+days = 2
+flow = [[]]*days
+flowData = [[]]*days
+for i in range (0, days):
+    fileFlow = "Data/Oct2017_Flow/pems_output_"  + str(i) + ".csv"
+    with open (fileFlow) as csvfile :
+        flowData.append(csv.reader(csvfile))
+        print (flowData)
+        for row in csvfile:
+            flow[i].append(row)
+
+print (flow[0])  
+print (flow[1]) 
+flow[0] = list(map(list, zip(*flow[0]))) 
+
+for i in range (0, days):
+    for row in flow[i] :
+        flowData[i].append(int(row[4]))
+        
