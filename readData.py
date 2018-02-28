179c32d0f63aeb31774feb4ff91fa9e2e0a008fc
raj-shukla <rajshukla@nevada.unr.edu>
2018-02-28 15:39:17 -0800

    data Read

--- readData.py
+++ readData.py
@@ -3,21 +3,29 @@ import csv
 
 
 days = 2
+flowList = [[]]*days
 flow = [[]]*days
-flowData = [[]]*days
-for i in range (0, days):
-    fileFlow = "Data/Oct2017_Flow/pems_output_"  + str(i) + ".csv"
-    with open (fileFlow) as csvfile :
-        flowData.append(csv.reader(csvfile))
-        print (flowData)
-        for row in csvfile:
-            flow[i].append(row)
-
-print (flow[0])  
-print (flow[1]) 
-flow[0] = list(map(list, zip(*flow[0]))) 
+postMile = []
+lanes = []
 
 for i in range (0, days):
-    for row in flow[i] :
-        flowData[i].append(int(row[4]))
+    flowFile = "Data/Oct2017_Flow/pems_output_"  + str(i) + ".csv"
+    with open (flowFile) as csvFile :
+        #print (flow)
+        next(csvFile)
+        csvReader = csv.reader(csvFile)
+        for row in csvReader:
+            flowList[i].append(row)    
+
+print(flowList[1][5])
+
+for i in range (0, days):
+    for row in flowList[i] :
+        #print(row[4])
+        flow[i].append(int(row[4]))
+
+for row in flowList[0]:
+    postMile.append(float(row[1]))
+    lanes.append(int(row[5]))
         
+print (lanes)
