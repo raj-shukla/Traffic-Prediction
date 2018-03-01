188d48ea71a4aeffcfccecb698365b6e59c4940d
Raj M Shukla <rshukla@0562118xb1nx1l.rd.unr.edu>
2018-03-01 00:41:17 -0800

    Data Read

--- readData.py
+++ readData.py
@@ -1,31 +1,51 @@
-import numpy as np
+#import numpy as np
 import csv
 
 
 days = 2
-flowList = [[]]*days
-flow = [[]]*days
+flowList = [[], []]
+flow = [[], []]
 postMile = []
 lanes = []
 
-for i in range (0, days):
-    flowFile = "Data/Oct2017_Flow/pems_output_"  + str(i) + ".csv"
-    with open (flowFile) as csvFile :
-        #print (flow)
-        next(csvFile)
-        csvReader = csv.reader(csvFile)
-        for row in csvReader:
-            flowList[i].append(row)    
 
-print(flowList[1][5])
+print(flow)
 
 for i in range (0, days):
-    for row in flowList[i] :
-        #print(row[4])
+    flowFile = "Data/testData/pems_output_"  + str(i) + ".csv"
+    csvFile = open (flowFile, "r")
+    #print (flow)
+    csvReader = csv.reader(csvFile)
+    next(csvReader)
+    print (csvReader)
+    for row in csvReader:
+	print (i)
+	print(flowList[i])
+        flowList[i].append(row) 
         flow[i].append(int(row[4]))
+	print (row) 
+	print (flowList) 
+
+
+
+
+
+
+
+
+
 
 for row in flowList[0]:
     postMile.append(float(row[1]))
     lanes.append(int(row[5]))
+print(len(flowList[0]))
+print(len(flowList[1]))
+print(len(flow[0]))
+print(len(flow[1]))
+print(len(lanes))
+print(len(postMile))
+
+print (flowList)
+print (flow)
         
-print (lanes)
+
