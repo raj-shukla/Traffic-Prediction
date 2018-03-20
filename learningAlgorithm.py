49aaaa94db2d61aeb1c3ff3d6e22a8d0bc1c54b7
raj-shukla <rajshukla@nevada.unr.edu>
2018-03-20 15:37:02 -0700

    read Data

--- learningAlgorithm.py
+++ learningAlgorithm.py
@@ -1,5 +1,6 @@
 import numpy as np
 import readData
+import predictionData
 
 days = readData.days
 flow = np.array(readData.flow)
@@ -11,7 +12,7 @@ timeSlot = 24*12
 flowAtPoint = np.empty((0, timeSlot))
 
 
-n = np.array([10, 5, 4, 5, 3, 1])
+n = np.array([5, 10, 20, 10, 10, 10, 1])
 L = np.size(n) - 1
 print (L)
 
@@ -30,59 +31,66 @@ for i  in range (0, L+1):
     dA.append([])
 
 
-data = np.array([[3, 4], [5, 2], [4, 5], [2, 7], [3, 2], [4, 2], [3, 5], [2, 1], [3, 4], [2, 6]])
-m = np.shape(data)[1]
+#data = np.array([[3, 4], [5, 2], [4, 5], [2, 7], [3, 2], [4, 2], [3, 5], [2, 1], [3, 4], [2, 6]])
+data = predictionData.inputArray  
 
-print (m)
 
 A[0] = data
 Z[0] = data
-Y = [[0.9,  0.7]]
+#Y = [[0.9,  0.7]]
+Y = predictionData.outputArray
 
 
 def InitializeWeight(layers, structure):
     initialWeight = [] 
     for l in range(0, layers):
-        tmpMatrix = np.random.rand(n[l+1], n[l])
+        tmpMatrix = (np.random.rand(n[l+1], n[l]))*10
         initialWeight.append(tmpMatrix)
     initialWeight.insert(0, None)
     return initialWeight
     
 def FindError(Output):
-    #print("#######################")
-    error = -A[L]*np.log (Y) - (1-A[L])*np.log(1 - np.asarray(Y))
+    #print(Y)
+    #print(1 - np.asarray(Y))
+    #error = -np.asarray(Y)*np.log (A[L]) - (1-np.asarray(Y))*np.log(1 - A[L])
+    error = np.sqrt((Y - A[L])*(Y-A[L]))
     meanError = np.mean(error)
-    #print(error)
-    #print(meanError)
     return meanError
     
 
 def ForwardPropagation(W, A, data):
     for l in range(1, L + 1):
         Z[l] = np.dot(W[l], A[l-1])
+        print(-Z[l])
         A[l] = 1/(1+ np.exp(-Z[l]))
+        #print("##############")
+        #print(A[l])
 
 def BackPropagation(W, A, data, alpha):
+    m = np.shape(data)[1]
     for l in range(L, 0, -1):
         if (l == L):
             dZ[l] = A[l] - Y
         else :
-            dZ[l] = dA[l]* Z[l]
+            dTerm = 1/(1+ np.exp(-Z[l]))
+            dZ[l] = dA[l]* (dTerm*(1-dTerm))
         dW[l] = (np.dot(dZ[l], A[l-1].T))/m
         dA[l-1] = np.dot(W[l].T, dZ[l])
-
         W[l] = W[l] - alpha*dW[l]
         
 
 W = InitializeWeight(L, n)
 print (W)
 alpha = 0.9
-for i in range(0, 1000):
-    ForwardPropagation(W, A, data)
-    cost = FindError(A)
-    print (cost)
-    BackPropagation(W, A, data, alpha)
-    alpha = alpha*0.9
+for i in range(0, 1):
+    for i in range(0, 800):
+        #print(data[:, i:i+5])
+        ForwardPropagation(W, A, data[:, i:i+20])
+        cost = FindError(A)
+        print("######################")
+        print (cost)
+        BackPropagation(W, A, data[:, i:i+20], alpha)
+        alpha = alpha*0.9
 print (W)
 
 
---
+++ predictionData.py
@@ -0,0 +1,57 @@
+import numpy as np
+import readData
+
+
+days = readData.days
+flow = np.array(readData.flow)
+flowList = np.array(readData.flowList)
+time = np.array(readData.time)
+postMile = np.array(readData.postMile)
+
+fPM = 35.78
+fTime = '12:00'
+timeSlot = 24*12
+points = 136
+flowAtPoint = np.empty((0, timeSlot))
+flowAtTime = np.empty((0, timeSlot ))
+print(np.max(flow))
+flow = (flow - np.min(flow))/(np.max(flow) - np.min(flow))
+print(flow)
+
+
+indexT = np.where(postMile == fPM)
+tmpArray = np.array([])
+for i in range (0, days):
+    for j, val in np.ndenumerate(indexT):
+        tmpArray= np.append(tmpArray, flow[i][val])
+    flowAtPoint = np.append(flowAtPoint, [tmpArray], axis=0)
+    tmpArray = np.array([])
+
+
+print (np.shape(flowAtPoint))
+
+inputArray = []
+outputArray = []
+nTimeSlot = 5
+pSlotTraffic = np.empty((0, nTimeSlot))
+nSlotTraffic = np.array([])
+for i in range(0, days):
+    for j in range(0, timeSlot + 1):
+        if (j + nTimeSlot + 1 >=timeSlot + 1):
+            break
+        pSlotTraffic = np.append(pSlotTraffic, [flowAtPoint[i][j:j+nTimeSlot]], axis=0)
+        nSlotTraffic = np.append(nSlotTraffic, flowAtPoint[i][j+nTimeSlot])
+        #print(j)
+        #print(j+nTimeSlot)
+        #print(flowAtPoint[i][j:j+nTimeSlot])
+        #print(flowAtPoint[i][j+nTimeSlot])
+
+pSlotTraffic = pSlotTraffic.T
+print (pSlotTraffic)
+print (nSlotTraffic)
+
+print (np.shape(pSlotTraffic))
+print (np.shape(nSlotTraffic))
+
+inputArray = pSlotTraffic
+outputArray = nSlotTraffic
